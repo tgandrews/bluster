fa378c3ef27b1af8aabdc0a5e0218516faa8d44f
Tom Andrews <git@tomandrews.co.uk>
2016-04-05 21:07:47 +0100

    Some basic tests of the server

---
+++ test/test_posts.js
@@ -0,0 +1,26 @@
+var http = require('http');
+
+var opts = {
+  host: 'localhost',
+  port: 6767,
+  method: 'GET',
+  path: '/posts',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+var timer;
+var req = http.request(opts, (res) => {
+  console.log("Status code", res.statusCode);
+  console.log(JSON.stringify(res.headers, null, 2));
+  res.setEncoding('utf8');
+  var buffer = '';
+  res.on('data', (chunk) => {
+    buffer += chunk;
+  });
+  res.on('end', () => {
+    console.log(buffer);
+  });
+});
+req.end();
